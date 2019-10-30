62df9edf201423e304b6bbb210408d57669d75b5
Aldo Neri <aldo_neri@outlook.com>
2019-10-30 11:16:33 -0600

    Avisos listos

--- src/app/pages/principal/principal.component.ts
+++ src/app/pages/principal/principal.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Usuario } from '../../models/usuario.model';
 import { Aviso } from '../../models/aviso.model';
 import { UsuarioService, InstitucionService, SettingsService, AvisosService } from '../../services/service.index';
+import { WebsocketService } from '../../services/websocket/websocket.service';
 
 // declare function init_plugins();
+declare function floating_labels();
+
+import Swal from 'sweetalert2';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-principal',
@@ -17,6 +23,8 @@ export class PrincipalComponent implements OnInit {
   usuario: Usuario;
   hora: number;
   titulo: string;
+
+  forma: FormGroup;
 
   cargando = true;
 
@@ -48,18 +56,40 @@ export class PrincipalComponent implements OnInit {
   constructor( public _usuarioService: UsuarioService,
                public _ajustes: SettingsService,
                public _institucionService: InstitucionService,
-               public _avisosService: AvisosService ) {
+               public _avisosService: AvisosService,
+               public _webSocketServive: WebsocketService ) {
     this.usuario = this._usuarioService.usuario;
   }
 
   ngOnInit() {
     // init_plugins();
 
+    floating_labels();
+    this.forma = new FormGroup({
+      titulo: new FormControl( null, Validators.required ),
+      aviso: new FormControl( null, Validators.required )
+    });
+
     this.hora = new Date().getHours();
     this.saludo();
     this.almacenamiento();
     this.cargarImagenesInicializar();
     this.cargarAvisos();
+    this.escucharSocket();
+  }
+
+  escucharSocket() {
+    this._webSocketServive.listen('cambio-aviso')
+        .subscribe( ( data: any ) => {
+          // console.log('Socket: ', data);
+
+          floating_labels();
+          this.hora = new Date().getHours();
+          this.saludo();
+          this.almacenamiento();
+          this.cargarImagenesInicializar();
+          this.cargarAvisos();
+        });
   }
 
   cargarImagenesInicializar() {
@@ -122,9 +152,63 @@ export class PrincipalComponent implements OnInit {
     this._avisosService.cargarAvisos()
           .subscribe( avisos => {
             this.avisos = avisos;
-            console.log(avisos);
+            // console.log(avisos);
             this.cargando = false;
           });
+
+  }
+
+  agregarAviso() {
+
+    if ( this.forma.invalid ) {
+      return;
+    }
+
+    let aviso = new Aviso(
+      this.forma.value.titulo,
+      this.forma.value.aviso
+    );
+
+    // console.log('AVISO: ', aviso);
+
+    this._avisosService.crearAviso( aviso )
+          .subscribe( resp => {
+            floating_labels();
+            this.hora = new Date().getHours();
+            this.saludo();
+            this.almacenamiento();
+            this.cargarImagenesInicializar();
+            this.cargarAvisos();
+          });
+
+  }
+
+  eliminarAviso( aviso: Aviso ) {
+
+    Swal.fire({
+      title: '¿Eliminar Aviso?',
+      type: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'No',
+      cancelButtonColor: '#e74c3c',
+      animation: false,
+      customClass: {
+        popup: 'animated tada'
+      }
+    }).then((eliminar) => {
+      if (eliminar.value) {
+        this._avisosService.eliminarAviso( aviso._id )
+          .subscribe( (resp: any) => {
+            floating_labels();
+            this.hora = new Date().getHours();
+            this.saludo();
+            this.almacenamiento();
+            this.cargarImagenesInicializar();
+            this.cargarAvisos();
+          } );
+      }
+    });
 
   }
 
